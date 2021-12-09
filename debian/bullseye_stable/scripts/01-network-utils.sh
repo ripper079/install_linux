d5b170774935e682cc9317dcce832c9b20c6d17c
ripper079 <44392667+ripper079@users.noreply.github.com>
2021-12-09 08:30:55 +0100

    wavemon added

--- debian/bullseye_stable/scripts/01-network-utils.sh
+++ debian/bullseye_stable/scripts/01-network-utils.sh
@@ -5,7 +5,8 @@
 # network-manager-gnome         - (gnome): network management framework (GNOME frontend)[systray applet]
 # wireless-tools                - Tools for manipulating Linux Wireless Extensions
 # wpasupplicant                 - client support for WPA and WPA2 (IEEE 802.11i)
-MY_NETWORK_TOOLS="network-manager network-manager-gnome wireless-tools wpasupplicant"
+# wavemon 						- (net): Wireless Device Monitoring Application 
+MY_NETWORK_TOOLS="network-manager network-manager-gnome wireless-tools wpasupplicant wavemon"
 
 echo ""
 echo "Installing...Network utils..."
