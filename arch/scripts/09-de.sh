24b340d70d604fa519ec36a865ab4c6acf34cb8f
ripper079 <44392667+ripper079@users.noreply.github.com>
2021-12-09 08:29:24 +0100

    Added media tools

--- arch/scripts/09-de.sh
+++ arch/scripts/09-de.sh
@@ -7,6 +7,9 @@
 # pcmanfm				- Extremely fast and lightweight file manager
 # ranger 				- Simple, vim-like file manager
 # vlc 					- Multi-platform MPEG, VCD/DVD, and DivX player
+# mpv 					- a free, open source, and cross-platform media player
+# ffmpeg 				- Complete solution to record, convert and stream audio and video
+# celluloid 			- Simple GTK+ frontend for mpv 
 # libreoffice-still		- LibreOffice maintenance branch
 # libreoffice-still-sv	- Swedish language pack for LibreOffice still
 # geeqie 				- Lightweight image viewer
@@ -19,7 +22,7 @@
 # xchm					- Viewer for CHM files (GUI front-end to chmlib)
 # gparted				- A Partition Magic clone, frontend to GNU Parted
 # Maybe GNUpg????
-MY_DESKTOP_EVVIRONMENT="firefox thunderbird pcmanfm ranger vlc libreoffice-still libreoffice-still-sv geeqie sxiv keepassxc rofi xarchiver copyq qpdfview xchm gparted"
+MY_DESKTOP_EVVIRONMENT="firefox thunderbird pcmanfm ranger vlc mpv ffmpeg celluloid libreoffice-still libreoffice-still-sv geeqie sxiv keepassxc rofi xarchiver copyq qpdfview xchm gparted"
 
 echo ""
 echo "Installing...custom Desktop Environment"
