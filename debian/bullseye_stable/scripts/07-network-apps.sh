1c557169099ba244256d8f96566f6c28e6926e78
ripper079 <44392667+ripper079@users.noreply.github.com>
2021-12-09 08:32:04 +0100

    rsynd and ssh added

--- debian/bullseye_stable/scripts/07-network-apps.sh
+++ debian/bullseye_stable/scripts/07-network-apps.sh
@@ -4,7 +4,9 @@
 
 # filezilla 		- (net): Full-featured graphical FTP/FTPS/SFTP client
 # qbittorrent 		- (net): bittorrent client based on libtorrent-rasterbar with a Qt5 GUI
-MY_NETWORK_APPS="filezilla qbittorrent"
+# rsync				- (net): fast, versatile, remote (and local) file-copying tool
+# ssh				- (net): secure shell client and server (metapackage) 
+MY_NETWORK_APPS="filezilla qbittorrent rsync ssh"
 
 echo ""
 echo "Installing...Network apps..."
