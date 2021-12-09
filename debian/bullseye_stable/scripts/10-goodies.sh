aa766461370eead2ef8f95fd9ec5e1b383b0bc6f
ripper079 <44392667+ripper079@users.noreply.github.com>
2021-12-09 08:33:43 +0100

    Flameshot added

--- debian/bullseye_stable/scripts/10-goodies.sh
+++ debian/bullseye_stable/scripts/10-goodies.sh
@@ -5,7 +5,8 @@
 # figlet 		- (text): Make large character ASCII banners out of ordinary text
 # cmatrix		- (misc): simulates the display from "The Matrix"
 # neofetch 		- (utils): Shows Linux System Information with Distribution Logo
-MY_GOODIES="figlet cmatrix neofetch"
+# flameshot 	- (graphics): Powerful yet simple-to-use screenshot software 
+MY_GOODIES="figlet cmatrix neofetch flameshot"
 
 echo ""
 echo "Installing...goodies"
